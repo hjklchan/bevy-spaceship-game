5d250af53b005f57d308b310f0efc684380d7b8f
Lucas chen <lucas.chen@gbo-asia.com>
2024-11-20 17:50:15 +0800

    add movement controls

--- src/spaceship.rs
+++ src/spaceship.rs
@@ -1,14 +1,23 @@
-use crate::{asset_loader::SceneAssets, movement::{Acceleration, Movement, MovementBundle, Velocity}};
+use crate::{
+    asset_loader::SceneAssets,
+    movement::{Acceleration, Movement, MovementBundle, Velocity},
+};
 use bevy::prelude::*;
 
 const INITIAL_TRANSLATION: Vec3 = Vec3::new(0.0, 0.0, -20.0);
 const INITIAL_VELOCITY: Vec3 = Vec3::new(0.0, 0.0, 1.0);
+const SPACESHIP_MOVEMENT_SPEED: f32 = 25.0;
+const SPACESHIP_ROTATION_SPEED: f32 = 2.5;
+const SPACESHIP_ROLL_SPEED: f32 = 2.5;
 
 #[derive(Bundle)]
 pub struct SpaceshipBundle {
     velocity: Velocity,
     model: SceneBundle,
 }
+
+#[derive(Component)]
+pub struct Spaceship;
 
 // ================================
 // ======= SpaceshipPlugin ========
@@ -18,11 +27,12 @@ pub struct SpaceshipPlugin;
 impl Plugin for SpaceshipPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(PostStartup, spawn_spaceship);
+        app.add_systems(Update, spaceship_movement_control);
     }
 }
 
 /// # spawn a new spaceship
-/// 
+///
 /// *TODO: create a new bundle to encapsulate the following components*,
 /// just like [`SpaceshipBundle`] structure.
 ///
@@ -40,9 +50,33 @@ fn spawn_spaceship(mut commands: Commands, scene_assets: Res<SceneAssets>) {
             ..Default::default()
         },
         MovementBundle {
-            velocity: INITIAL_VELOCITY.into(),
+            velocity: Default::default(),
             acceleration: Acceleration::new(Vec3::ZERO),
             mark: Movement,
         },
+        Spaceship,
     ));
+}
+
+fn spaceship_movement_control(
+    mut query: Query<(&mut Transform, &mut Velocity), (With<Movement>, With<Spaceship>)>,
+    button_input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+) {
+    // ensure the player still alive
+    if let Ok((mut transform, mut velocity)) = query.get_single_mut() {
+        let rotation = 0.0;
+        let roll = 0.0;
+        let mut movement = 0.0;
+
+        if button_input.pressed(KeyCode::KeyW) {
+            movement = SPACESHIP_MOVEMENT_SPEED;
+        } else if button_input.pressed(KeyCode::KeyS) {
+            movement = -SPACESHIP_MOVEMENT_SPEED;
+        } else {
+            movement = 0.0;
+        }
+
+        velocity.value = -transform.forward() * movement;
+    }
 }
